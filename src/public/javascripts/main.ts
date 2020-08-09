081cf101a2b89a608e1a37d24a848e45153aa7c8
Richard Kasendu <kasendu@gmail.com>
2020-08-09 23:23:43 +0200

    Cleanup.

--- src/public/javascripts/main.ts
+++ src/public/javascripts/main.ts
@@ -1,53 +1,58 @@
 import * as THREE from 'three';
-import { CompressedTexture, PerspectiveCamera, Scene, WebGLRenderer, Texture, MeshStandardMaterial, PlaneGeometry } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+import dat, { GUI } from "three/examples/jsm/libs/dat.gui.module";
+import index from 'three-dat.gui';
 
 const scene: THREE.Scene = new THREE.Scene()
-//scene.background = new THREE.Color(0xff0000)
 
+// Extending global namespace Window so that Three.js Inspector can recgonize it
 declare global {
     interface Window { 
 		scene: any;
 		THREE: any;
 	 }
 }
-
 window.scene = scene;
 window.THREE = THREE;
 
+// An axis object to visualize the 3 axes in a simple way.
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
+/*
+A light that gets emitted from a single point in all directions. 
+A common use case for this is to replicate the light emitted from a bare lightbulb.
+*/
 const light = new THREE.PointLight(0xffffff, 2);
 light.position.set(0, 5, 10);
 scene.add(light);
 
+// Setting up camera
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 3
 
+// Setting up renderer
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+// Setting up controls
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
-//controls.addEventListener('change', render)
 
-// 5632 2048
+//Setting up live stats
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(5632, 2048, 100, 100)
-
 const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
 
-//const texture = new THREE.TextureLoader().load("img/grid.png")
 const texture = new THREE.TextureLoader().load("images/provinces.bmp")
 material.map = texture
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_eso0932a.jpg", "img/nx_eso0932a.jpg", "img/py_eso0932a.jpg", "img/ny_eso0932a.jpg", "img/pz_eso0932a.jpg", "img/nz_eso0932a.jpg"])
-// envTexture.mapping = THREE.CubeReflectionMapping
-// material.envMap = envTexture
 
-//const specularTexture = new THREE.TextureLoader().load("img/earthSpecular.jpg")
-// material.specularMap = specularTexture
+const normalTexture = new THREE.TextureLoader().load("images/world_normal.bmp")
+material.normalMap = normalTexture
 
 const displacementMap = new THREE.TextureLoader().load("images/heightmap.bmp")
 material.displacementMap = displacementMap
@@ -55,7 +60,9 @@ material.displacementMap = displacementMap
 const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
 scene.add(plane)
 
-camera.position.z = 3
+
+const gui = new dat.GUI()
+
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -65,75 +72,9 @@ function onWindowResize() {
     render()
 }
 
-const stats = Stats()
-document.body.appendChild(stats.dom)
-
-var options = {
-    side: {
-        "FrontSide": THREE.FrontSide,
-        "BackSide": THREE.BackSide,
-        "DoubleSide": THREE.DoubleSide,
-    }
+function render() {
+    renderer.render(scene, camera)
 }
-const gui = new GUI()
-
-const materialFolder = gui.addFolder('THREE.Material')
-materialFolder.add(material, 'transparent')
-materialFolder.add(material, 'opacity', 0, 1, 0.01)
-materialFolder.add(material, 'depthTest')
-materialFolder.add(material, 'depthWrite')
-materialFolder.add(material, 'alphaTest', 0, 1, 0.01).onChange(() => updateMaterial())
-materialFolder.add(material, 'visible')
-materialFolder.add(material, 'side', options.side).onChange(() => updateMaterial())
-//materialFolder.open()
-
-var data = {
-    color: material.color.getHex(),
-    emissive: material.emissive.getHex(),
-    specular: material.specular.getHex()
-};
-
-var meshPhongMaterialFolder = gui.addFolder('THREE.meshPhongMaterialFolder');
-
-meshPhongMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) })
-meshPhongMaterialFolder.addColor(data, 'emissive').onChange(() => { material.emissive.setHex(Number(data.emissive.toString().replace('#', '0x'))) })
-meshPhongMaterialFolder.addColor(data, 'specular').onChange(() => { material.specular.setHex(Number(data.specular.toString().replace('#', '0x'))) });
-meshPhongMaterialFolder.add(material, 'shininess', 0, 1024);
-meshPhongMaterialFolder.add(material, 'wireframe')
-meshPhongMaterialFolder.add(material, 'flatShading').onChange(() => updateMaterial())
-meshPhongMaterialFolder.add(material, 'reflectivity', 0, 1)
-meshPhongMaterialFolder.add(material, 'refractionRatio', 0, 1)
-meshPhongMaterialFolder.add(material, 'displacementScale', -1, 1, 0.01)
-meshPhongMaterialFolder.add(material, 'displacementBias', -1, 1, 0.01)
-meshPhongMaterialFolder.open()
-
-var planeData = {
-    width: 3.6,
-    height: 1.8,
-    widthSegments: 360,
-    heightSegments: 180
-};
-const planePropertiesFolder = gui.addFolder("PlaneGeometry")
-//planePropertiesFolder.add(planeData, 'width', 1, 30).onChange(regeneratePlaneGeometry)
-//planePropertiesFolder.add(planeData, 'height', 1, 30).onChange(regeneratePlaneGeometry)
-planePropertiesFolder.add(planeData, 'widthSegments', 1, 360).onChange(regeneratePlaneGeometry)
-planePropertiesFolder.add(planeData, 'heightSegments', 1, 180).onChange(regeneratePlaneGeometry)
-planePropertiesFolder.open()
-
-function regeneratePlaneGeometry() {
-    let newGeometry = new THREE.PlaneGeometry(
-        planeData.width, planeData.height, planeData.widthSegments, planeData.heightSegments
-    )
-    plane.geometry.dispose()
-    plane.geometry = newGeometry
-}
-
-
-function updateMaterial() {
-    material.side = Number(material.side)
-    material.needsUpdate = true
-}
-
 var animate = function () {
     requestAnimationFrame(animate)
 
@@ -141,8 +82,4 @@ var animate = function () {
 
     stats.update()
 };
-
-function render() {
-    renderer.render(scene, camera)
-}
 animate();
