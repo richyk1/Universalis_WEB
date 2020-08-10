420020adfb8b079ed3b8af6cb3c47521a9204443
Richard Kasendu <kasendu@gmail.com>
2020-08-10 02:32:57 +0200

    First draft for light placement

--- src/public/javascripts/main.ts
+++ src/public/javascripts/main.ts
@@ -1,8 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls, MapControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import dat, { GUI } from "three/examples/jsm/libs/dat.gui.module";
-import index from 'three-dat.gui';
+import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -16,21 +15,17 @@ declare global {
 window.scene = scene;
 window.THREE = THREE;
 
-// An axis object to visualize the 3 axes in a simple way.
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
-
 /*
 A light that gets emitted from a single point in all directions. 
 A common use case for this is to replicate the light emitted from a bare lightbulb.
 */
-const light = new THREE.PointLight(0xffffff, 2);
-light.position.set(0, 5, 10);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1000);
 scene.add(light);
 
 // Setting up camera
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 3
+camera.position.z = 1000
 
 // Setting up renderer
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
@@ -38,17 +33,21 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 // Setting up controls
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new MapControls(camera, renderer.domElement)
+controls.enableRotate = false
 controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
+controls.minDistance = 250
+controls.maxDistance = 1000
 
 //Setting up live stats
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(5632, 2048, 100, 100)
+const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(5632, 2048, 5632 / 8, 2048 / 8)
 const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
 
 const texture = new THREE.TextureLoader().load("images/provinces.bmp")
+
 material.map = texture
 
 const normalTexture = new THREE.TextureLoader().load("images/world_normal.bmp")
@@ -60,26 +59,32 @@ material.displacementMap = displacementMap
 const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
 scene.add(plane)
 
+// Graphical User Interface for value tweaking
+const gui = new GUI()
+const mapFolder = gui.addFolder("Map")
 
-const gui = new dat.GUI()
+mapFolder.open()
+const cameraFolder = gui.addFolder("Camera")
+cameraFolder.add(camera.position, "z", 0, 1000, 0.01)
+cameraFolder.open()
+const lightFolder = gui.addFolder("Light")
 
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
+// Event handling
+window.addEventListener('resize', () => 
+{
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
+}, undefined)
 
-function render() {
-    renderer.render(scene, camera)
-}
+
+// Rendering
 var animate = function () {
     requestAnimationFrame(animate)
 
-    render()
+    renderer.render(scene, camera)
 
+    gui.updateDisplay()
     stats.update()
 };
 animate();
