adc154c6d88de3e3a95c96f17dbaf43d49b293c6
richyk1 <kasendu@gmail.com>
2020-08-24 13:40:19 +0200

    Cleanup

--- src/public/javascripts/main.ts
+++ src/public/javascripts/main.ts
@@ -3,15 +3,16 @@ import { OrbitControls, MapControls } from 'three/examples/jsm/controls/OrbitCon
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 import * as Tiles from "./tiles";
+import { Vector3 } from 'three';
 
 const scene: THREE.Scene = new THREE.Scene()
 
 // Extending global namespace Window so that Three.js Inspector can recgonize it
 declare global {
-    interface Window { 
-		scene: any;
-		THREE: any;
-	 }
+    interface Window {
+        scene: any;
+        THREE: any;
+    }
 }
 window.scene = scene;
 window.THREE = THREE;
@@ -24,9 +25,9 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1000);
 scene.add(light);
 scene.background = new THREE.Color("white")
+
 // Setting up camera
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
 camera.position.z = 1000
 
 // Setting up renderer
@@ -38,7 +39,7 @@ document.body.appendChild(renderer.domElement)
 const controls = new MapControls(camera, renderer.domElement)
 controls.enableRotate = false
 controls.screenSpacePanning = true //so that panning up and down doesn't zoom in/out
-controls.minDistance = 250
+controls.minDistance = 50
 controls.maxDistance = 1000
 
 //Setting up live stats
@@ -46,67 +47,110 @@ const stats = Stats()
 document.body.appendChild(stats.dom)
 
 
-/*
-const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(5632, 2048, 5632 / 8, 2048 / 8)
-const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
-
-const texture = new THREE.TextureLoader().load("images/provinces.bmp")
-
-material.map = texture
-
-const normalTexture = new THREE.TextureLoader().load("images/world_normal.bmp")
-material.normalMap = normalTexture
-
-const displacementMap = new THREE.TextureLoader().load("images/heightmap.bmp")
-material.displacementMap = displacementMap
-
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
-
-scene.add(plane)
-*/
-
 
 // Event handling
-window.addEventListener('resize', () => 
-{
+window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
 }, undefined)
 
 
-const tiles: Tiles.IPoint[][] = Tiles.GetTiles()
-
 // Loading map
-var shapes: THREE.Shape[] = []
-const test: Tiles.IPoint[][] = []
-test.push(tiles[0])
-for(const tile of tiles)
-{
-    const shape = new THREE.Shape();
-    shape.moveTo(tile[0].x, window.innerHeight - tile[0].y)
-    for (var i = 1; i < tile.length; i++)
-        shape.lineTo(tile[i].x, window.innerHeight - tile[i].y)
-    shape.moveTo(tile[0].x, window.innerHeight - tile[0].y)
-    
+const tiles: Tiles.Tile[] = Tiles.GetTiles()
+const test: Tiles.Tile[] = []
+for (var i = 0; i < 10; i++)
+    test.push(tiles[i])
 
-    shapes.push(shape)
+function DrawShapes(tiles: Tiles.Tile[]) {
+
+    for (const tile of tiles) {
+        var group: THREE.Group = new THREE.Group();
+        var shapeMat = new THREE.MeshBasicMaterial({
+            color: Math.random() * 0xffffff
+        });
+        for (const border of tile.borders) {
+            const shape = new THREE.Shape();
+            shape.moveTo(border.points[0].x, window.innerHeight - border.points[0].y)
+            for (var i = 1; i < border.points.length; i++)
+                shape.lineTo(border.points[i].x, window.innerHeight - border.points[i].y)
+            shape.moveTo(border.points[0].x, window.innerHeight -  border.points[0].y)
+            
+
+            var shapeGeometry = new THREE.ShapeBufferGeometry(shape)
+            var shapeMesh = new THREE.Mesh(shapeGeometry, shapeMat)
+            shapeMesh.frustumCulled = true;
+            group.add(shapeMesh);
+            shapeMesh.material.color = new THREE.Color(0x333333);
+        }
+        group.visible = true;
+        scene.add(group);
+    }
+}
+    /*
+
+function DrawLines(tiles: Tiles.Tile[]) {
+    var points: THREE.Vector3[] = []
+    for (const tile of test) {
+        const shape = new THREE.Shape();
+        points.push(new THREE.Vector3(tile[0].x, window.innerHeight - tile[0].y, 1))
+        for (var i = 1; i < tile.length; i++)
+            points.push(new THREE.Vector3(tile[i].x, window.innerHeight - tile[i].y, 1))
+        points.push(new THREE.Vector3(tile[0].x, window.innerHeight - tile[0].y, 1))
+    }
+
+    var pointGeometry = new THREE.BufferGeometry().setFromPoints(points);
+    var material = new THREE.LineBasicMaterial({ color: Math.random() * 0xffffff });
+    var finalShape = new THREE.Line(pointGeometry, material);
+
+    scene.add(finalShape);
 }
 
-var shapeGeometry = new THREE.ShapeBufferGeometry(shapes)
-var shapeMat = new THREE.MeshBasicMaterial({
-    color: Math.random() * 0xffffff
-});
+function DrawTestLines(tiles: Tiles.Tile[]) {
+    for (const tile of tiles) {
+        var points: THREE.Vector2[] = []
+        points.push(new THREE.Vector2(tile[0].x, window.innerHeight - tile[0].y))
+        for (var i = 1; i < tile.length; i++)
+            points.push(new THREE.Vector2(tile[i].x, window.innerHeight - tile[i].y))
+        points.push(new THREE.Vector2(tile[0].x, window.innerHeight - tile[0].y))
 
-var finalShape = new THREE.Mesh(shapeGeometry, shapeMat)
+
+        var pointGeometry = new THREE.BufferGeometry().setFromPoints(points);
+        var material = new THREE.LineBasicMaterial({ color: Math.random() * 0xffffff });
+        var finalShape = new THREE.Line(pointGeometry, material);
+
+        scene.add(finalShape);
+    }
+}
 
 
-scene.add(finalShape);
+*/
+function DrawMap() {
+
+    const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(5632, 2048, 5632 / 8, 2048 / 8)
+    const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
+
+    const texture = new THREE.TextureLoader().load("images/provinces.bmp")
+
+    material.map = texture
+
+    const normalTexture = new THREE.TextureLoader().load("images/world_normal.bmp")
+    material.normalMap = normalTexture
+
+    const displacementMap = new THREE.TextureLoader().load("images/heightmap.bmp")
+    material.displacementMap = displacementMap
+
+    const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material, )
+    plane.position.set(0, 0, 0)
+
+    scene.add(plane)
+}
+
+DrawShapes(tiles);
 
 // Graphical User Interface for value tweaking
 const gui = new GUI()
 const mapFolder = gui.addFolder("Map")
-mapFolder.add(finalShape.position, "z", 0, 1000, 0.01)
 mapFolder.open()
 const cameraFolder = gui.addFolder("Camera")
 cameraFolder.add(camera.position, "z", 0, 1000, 0.01)
--- src/public/javascripts/tiles.ts
+++ src/public/javascripts/tiles.ts
@@ -1,40 +1,40 @@
 // All operations related to province handling
-import { provinces } from "./tiles.json";
+import { tiles } from "./tiles.json";
 
-export interface ITile
-{
-    readonly points: IPoint[]
+export interface Tile {
+  readonly name: string,
+  readonly borders: Border[]
 }
 
-export interface IPoint
-{
-    readonly x: number,
-    readonly y: number,
+export interface Border {
+  readonly points: Point[]
 }
 
-var tiles: IPoint[][] = []
-export function GetTiles(): IPoint[][] {
-    if(tiles.length == 0)
-    {
-        for (const tile in provinces)
-        {
-            var points: IPoint[] = []
-            var counter: number = 1
-            for (const key of Object.keys(provinces[tile])) {
-                for(const point in provinces[tile][key])
-                {
-                    points.push(
-                        {
-                            x: Number(provinces[tile][key][point][0]),
-                            y: Number(provinces[tile][key][point][1])
-                        }
-                    )
-                }
-             }
-            
-            counter++
-            tiles.push(points)
+export interface Point {
+  readonly x: number,
+  readonly y: number
+}
+
+var tilesGrouped: Tile[] = []
+export function GetTiles(): Tile[] {
+  if (tilesGrouped.length == 0) {
+    for (const tileIndex in tiles) {
+      var tileBorders: Border[] = [];
+      for (const border of tiles[tileIndex]["borders"]) {
+        var borderPoints: Point[] = []
+        for (const point of border) {
+          borderPoints.push({x: point[0], y: point[1]})
         }
+        tileBorders.push({points: borderPoints});
+      }
+
+      var tile: Tile = {
+        name: tiles[tileIndex]["name"],
+        borders: tileBorders
+      }
+
+      tilesGrouped.push(tile)
     }
-    return tiles
+  }
+  return tilesGrouped
 }
